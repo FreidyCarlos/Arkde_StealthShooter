5f882723837d4e915de688d963d266c9b3e21785
freidylcarlos <freidylcarlos@gmail.com>
2025-07-24 21:57:59 -0500

    ** Player Inputs

---
+++ Source/ARK_StealthShooter/Private/Player/STS_PlayerCharacter.cpp
@@ -0,0 +1,50 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/STS_PlayerCharacter.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+
+ASTS_PlayerCharacter::ASTS_PlayerCharacter()
+{
+	SpringArmComponent = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComponent"));
+	SpringArmComponent->SetupAttachment(RootComponent);
+	SpringArmComponent->bUsePawnControlRotation = true; //Allows to assign the object to which the character's rotation reacts
+
+	CameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComponent"));
+	CameraComponent->SetupAttachment(SpringArmComponent);
+}
+
+void ASTS_PlayerCharacter::MoveFoward(float AxisValue)
+{
+	AddMovementInput(GetActorForwardVector() * AxisValue);
+}
+
+void ASTS_PlayerCharacter::MoveRight(float AxisValue)
+{
+	AddMovementInput(GetActorRightVector() * AxisValue);
+}
+
+void ASTS_PlayerCharacter::StartCrouch()
+{
+	Crouch();
+}
+
+void ASTS_PlayerCharacter::EndCrouch()
+{
+	UnCrouch();
+}
+
+void ASTS_PlayerCharacter::SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &ASTS_PlayerCharacter::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("LookSide", this, &ASTS_PlayerCharacter::AddControllerYawInput);
+
+	PlayerInputComponent->BindAxis("MoveFoward", this, &ASTS_PlayerCharacter::MoveFoward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &ASTS_PlayerCharacter::MoveRight);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &ASTS_PlayerCharacter::StartCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &ASTS_PlayerCharacter::EndCrouch);
+}
---
+++ Source/ARK_StealthShooter/Public/Player/STS_PlayerCharacter.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "STS_BaseCharacter.h"
+#include "STS_PlayerCharacter.generated.h"
+
+class UCameraComponent;
+class USpringArmComponent;
+
+UCLASS()
+class ARK_STEALTHSHOOTER_API ASTS_PlayerCharacter : public ASTS_BaseCharacter
+{
+	GENERATED_BODY()
+
+public:
+
+	ASTS_PlayerCharacter();
+
+protected:
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	UCameraComponent* CameraComponent;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Components")
+	USpringArmComponent* SpringArmComponent;
+
+protected:
+
+	void MoveFoward(float AxisValue);
+
+	void MoveRight(float AxisValue);
+
+	void StartCrouch();
+
+	void EndCrouch();
+
+public:
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+};
