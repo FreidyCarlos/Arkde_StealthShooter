4048c8ab5c784d47e020b8776c526b6b2495a37b
freidylcarlos <freidylcarlos@gmail.com>
2025-08-01 20:38:40 -0500

    ** Crowd Management

--- Source/ARK_StealthShooter/Private/Enemy/AI/STS_AIController.cpp
+++ Source/ARK_StealthShooter/Private/Enemy/AI/STS_AIController.cpp
@@ -3,8 +3,10 @@
 
 #include "Enemy/AI/STS_AIController.h"
 #include "Components/STS_HealthComponent.h"
+#include "Navigation/CrowdFollowingComponent.h"
 
-ASTS_AIController::ASTS_AIController()
+ASTS_AIController::ASTS_AIController(const FObjectInitializer& ObjectInitializer)
+	:Super(ObjectInitializer.SetDefaultSubobjectClass<UCrowdFollowingComponent>(TEXT("PathFollowingComponent")))
 {
 	SetGenericTeamId(FGenericTeamId(1));
 }
